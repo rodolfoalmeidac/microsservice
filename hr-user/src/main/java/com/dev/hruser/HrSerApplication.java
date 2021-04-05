f38de1ae36badc06a7beb873ac6f7dc915259bbe
rodolfoalmeidac <rodolfoo30@gmail.com>
2021-04-05 16:46:13 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/dev/hruser/HrSerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrSerApplication.class, args);
+	}
+
+}
