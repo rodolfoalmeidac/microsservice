9f4597d9c94e73071ea32bea10085e0b13ebb183
rodolfoalmeidac <rodolfoo30@gmail.com>
2021-04-05 21:38:46 -0300

    Project hr-oauth created

--- hr-worker/src/main/java/com/dev/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/dev/hrworker/resources/WorkerResource.java
@@ -23,8 +23,8 @@ public class WorkerResource {
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
+//	@Value("${test.config}")
+//	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -34,7 +34,7 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
+		logger.info("CONFIG = " + "Mock pois o git tem dupla verificação");
 		return ResponseEntity.noContent().build();
 	}
 	
