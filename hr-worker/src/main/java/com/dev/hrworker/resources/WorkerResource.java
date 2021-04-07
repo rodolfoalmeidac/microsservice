17c3e73599167bad686a2df7e97d8916b6425b60
rodolfoalmeidac <rodolfoo30@gmail.com>
2021-04-07 11:04:03 -0300

    Db config server

--- hr-worker/src/main/java/com/dev/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/dev/hrworker/resources/WorkerResource.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -47,12 +46,12 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		logger.info("Port = " + env.getProperty("local.server.port"));
 		
